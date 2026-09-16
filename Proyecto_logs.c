025e45a49d73627d6a4ba2c46b0a478a85068945
Carlos Campos Alday <carlos.campos.alday@estudiantxs.uacm.edu.mx>
2026-09-16 17:43:39 -0600

    Agregar Proyecto_logs

---
+++ Proyecto_logs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//======
+//2. Constante (#define)
+//======
+#define NUM_EQUIPOS 5
+#define x 3
+#define NUM_EVVENTOS 27
+
+//=======
+//3. ENUM (tipos de eventos)
+//=======
+enum TipoEvento
+int main(){
+
+
+}
